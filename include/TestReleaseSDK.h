c180ed5fde3dad8f4330ec5b920861734efb73d8
ZXQiao <xiuqiao.zhang@xinmei365.com>
2017-03-14 15:49:08 +0800

    init git

---
+++ include/TestReleaseSDK.h
@@ -0,0 +1,13 @@
+//
+//  TestReleaseSDK.h
+//  ReleaseToCocopods
+//
+//  Created by Qiao on 2017/3/13.
+//  Copyright © 2017年 xinmei365. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestReleaseSDK : NSObject
+- (void)printSomething;
+@end
